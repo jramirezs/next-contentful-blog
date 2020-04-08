48db6a54832d497eea3f45a7d74bb450561e1da2
Jorge Ramírez Serrato <jramirezserrato@gmail.com>
2020-04-08 22:33:10 +0300

    run purgeCSS only for production

--- postcss.config.js
+++ postcss.config.js
@@ -5,18 +5,20 @@ module.exports = {
     'autoprefixer',
     [
       '@fullhuman/postcss-purgecss',
-      {
-        // Specify the paths to all of the template files in your project
-        content: ['./src/**/*.tsx'],
+      process.env.NODE_ENV === 'production'
+        ? {
+            // Specify the paths to all of the template files in your project
+            content: ['./src/**/*.tsx'],
 
-        // make sure css reset isnt removed on html and body
-        whitelist: ['html', 'body'],
+            // make sure css reset isnt removed on html and body
+            whitelist: ['html', 'body'],
 
-        whitelistPatterns: [/(^|\.)fa-/, /-fa($|\.)/],
+            whitelistPatterns: [/(^|\.)fa-/, /-fa($|\.)/],
 
-        // Include any special characters you're using in this regular expression
-        defaultExtractor: content => content.match(/[A-za-z0-9-_:/]+/g) || [],
-      },
+            // Include any special characters you're using in this regular expression
+            defaultExtractor: content => content.match(/[A-za-z0-9-_:/]+/g) || [],
+          }
+        : false,
     ],
   ],
 };
